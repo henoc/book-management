053ce97f384dd4f7e84a032e13f0b2eab199af59
henoc <agekutar@gmail.com>
2024-08-10 18:14:23 +0900

    Book修正

--- src/main/kotlin/com/example/bookmanagement/entity/Book.kt
+++ src/main/kotlin/com/example/bookmanagement/entity/Book.kt
@@ -15,10 +15,13 @@ import jakarta.validation.constraints.NotNull
  */
 data class Book(
     val id: Int? = null,
+
     @field:NotBlank(message = "Title cannot be blank")
     val title: String,
+
     @field:Min(value = 0, message = "Publication year must be non-negative")
     val publicationYear: Int?,
+
     @field:NotNull(message = "Author ID is required")
     @field:Min(value = 1, message = "Author ID must be positive")
     val authorId: Int
